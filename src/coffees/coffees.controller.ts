2f718a253dd4e92687c3a84a205ec21aaf2fc089
Anthony A.S Baptiste <anthony.baptiste@outlook.com>
2022-09-23 22:23:25 -0400

    Basic Api implemented

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
+  @Get()
+  findall(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body) {
+    return this.coffeeService.create(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.coffeeService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
+  }
+}
